9cbc55686ae9584390687c51f1b9c759ad13f3b3
abhrajitray77 <abhrajitray77@gmail.com>
2023-03-10 22:40:59 +0530

    navbar added || first commit

---
+++ components/Banner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ParallaxProvider } from 'react-scroll-parallax'
+
+const Banner = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Banner
---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="w-screen flex justify-end px-10">
+        <ul className="flex space-x-10 py-8 text-black font-bold">
+          <li>About Me</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
