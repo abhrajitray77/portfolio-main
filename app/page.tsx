cc8e36dbcad279c3d2f43e6c486cf94bfde67ccb
abhrajitray77 <abhrajitray77@gmail.com>
2023-03-15 01:20:22 +0530

    technologies section added

--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,11 @@ export default function Home() {
               alt="propic"
             />
           </div>
-          <div className="absolute bottom-0 md:left-[400px] md:top-20 lg:left-[450px] lg:top-20 flex-col 
-          font-mono leading-none font-extrabold">
-              <h1 className="text-[4rem] md:text-[6rem]">ABHRAJIT</h1>
+          <div
+            className="absolute bottom-0 md:left-[400px] md:top-20 lg:left-[450px] lg:top-20 flex-col 
+          font-mono leading-none font-extrabold"
+          >
+            <h1 className="text-[4rem] md:text-[6rem]">ABHRAJIT</h1>
             <h1 className="text-[3.8rem] md:text-[5.8rem]">RAY</h1>
           </div>
         </div>
@@ -33,13 +35,15 @@ export default function Home() {
           <div className="relative flex flex-col">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
               Welcome to my little corner of the web{" "}
-              <span className="text-[40px] md:text-[50px] lg:text-[60px]">!</span>
+              <span className="text-[40px] md:text-[50px] lg:text-[60px]">
+                !
+              </span>
             </h1>
-            <p className="text-2xl font-medium pt-10 leading-relaxed ">
+            <p className="md:text-2xl font-medium pt-10 leading-relaxed ">
               I&apos;m a frontend web developer who specializes in creating fast
               and scalable websites with responsive design and great UI.
               <br />
-              I use Tailwind CSS, React, and Next.js, along with technologies
+              I use Next.js, React, and Tailwind CSS, along with technologies
               like Firebase, Supabase and MongoDB.
               <br />
               <br />
@@ -53,18 +57,73 @@ export default function Home() {
 
         {/* Skills */}
 
-        <section className="h-auto w-auto">
+        <section className="h-auto w-auto mt-20">
           <div>
-            <h1>Technologies that I love working with --&gt;</h1>
+            <h1 className="custom-h1">
+              Technologies that I love working with --&gt;
+            </h1>
+            <div className=" grid grid-cols-2 place-content-center gap-4 mt-12">
+              <Image
+                className="w-40"
+                alt=""
+                src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
+                width={300}
+                height={100}
+              />
+              <Image
+                className="w-40"
+                alt=""
+                src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+                width={300}
+                height={100}
+              />
+              <Image
+                className="w-40"
+                alt=""
+                src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
+                width={300}
+                height={100}
+              />
+              <Image
+                className="w-40"
+                alt=""
+                src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
+                width={300}
+                height={100}
+              />
+              <Image
+                className="w-40"
+                alt=""
+                src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
+                width={300}
+                height={100}
+              />
+              <Image
+                className="w-40"
+                alt=""
+                src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase"
+                width={300}
+                height={100}
+              />
+              <Image
+                className="w-40"
+                alt=""
+                src="https://img.shields.io/badge/Supabase-3ECF8E?style=for-the-badge&logo=supabase&logoColor=white"
+                width={300}
+                height={100}
+              />
+
+            </div>
           </div>
-          <div>
-            <h1>Skills that come in handy --&gt;</h1>
+          <div className="mt-12">
+            <h1 className="custom-h1">Skills that come in handy --&gt;</h1>
+            <div></div>
           </div>
         </section>
 
         {/* Projects */}
 
-        <section></section>
+        <section className="h-auto w-auto mt-20"></section>
 
         {/* Contact */}
 
