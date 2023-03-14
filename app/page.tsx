b642e6ea763cec04be7f64777feebe4ed498a394
abhrajitray77 <abhrajitray77@gmail.com>
2023-03-15 01:28:45 +0530

    technologies part done

--- app/page.tsx
+++ app/page.tsx
@@ -62,51 +62,52 @@ export default function Home() {
             <h1 className="custom-h1">
               Technologies that I love working with --&gt;
             </h1>
-            <div className=" grid grid-cols-2 place-content-center gap-4 mt-12">
+            <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 place-content-center gap-4 mt-12
+            place-items-start ">
               <Image
-                className="w-40"
+                className="w-40 my-auto"
                 alt=""
                 src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
                 width={300}
                 height={100}
               />
               <Image
-                className="w-40"
+                className="w-40 my-auto"
                 alt=""
                 src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
                 width={300}
                 height={100}
               />
               <Image
-                className="w-40"
+                className="w-40 my-auto"
                 alt=""
                 src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"
                 width={300}
                 height={100}
               />
               <Image
-                className="w-40"
+                className="w-40 my-auto"
                 alt=""
                 src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"
                 width={300}
                 height={100}
               />
               <Image
-                className="w-40"
+                className="w-40 my-auto"
                 alt=""
                 src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white"
                 width={300}
                 height={100}
               />
               <Image
-                className="w-40"
+                className="w-40 my-auto"
                 alt=""
                 src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=for-the-badge&logo=firebase"
                 width={300}
                 height={100}
               />
               <Image
-                className="w-40"
+                className="w-40 my-auto"
                 alt=""
                 src="https://img.shields.io/badge/Supabase-3ECF8E?style=for-the-badge&logo=supabase&logoColor=white"
                 width={300}
