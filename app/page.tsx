7bea72844fdbe2decf9d4eed871432799c882745
abhrajitray77 <abhrajitray77@gmail.com>
2023-03-19 00:06:44 +0530

    Contact section added

--- app/page.tsx
+++ app/page.tsx
@@ -226,7 +226,7 @@ export default function Home() {
                 "React.js",
                 "TailwindCSS",
                 "Framer-Motion",
-                "React-Router"
+                "React-Router",
               ]}
             />
             <ProjCard
@@ -235,18 +235,68 @@ export default function Home() {
               imgSrc={netpic}
               altText="Netflix Clone"
               projLink="https://netflix-learn.vercel.app/"
-              tech={[
-                "Next.js",
-                "TailwindCSS"
-              ]}
+              tech={["Next.js", "TailwindCSS"]}
             />
           </div>
         </section>
 
         {/* Contact */}
 
-        <section>
-          <h1 className="custom-h1 mt-28 flex justify-start">Contact</h1>
+        <section className="flex-col space-y-3">
+          <h1 className="custom-h1 mt-20 lg:mt-28 flex justify-start mb-8">
+            Reach Out To Me
+          </h1>
+           {/*    email */}
+          <div className="space-y-2">
+            <h3 className="text-xl font-medium">Send an E-Mail</h3>
+            <div className="contact-item">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-8 mt-1"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M22.288 21h-20.576c-.945 0-1.712-.767-1.712-1.712v-13.576c0-.945.767-1.712 1.712-1.712h20.576c.945 0 1.712.767 1.712 1.712v13.576c0 .945-.767 1.712-1.712 1.712zm-10.288-6.086l-9.342-6.483-.02 11.569h18.684v-11.569l-9.322 6.483zm8.869-9.914h-17.789l8.92 6.229s6.252-4.406 8.869-6.229z" />
+              </svg>
+              <span className="">abhrajitray77@gmail.com</span>
+            </div>
+          </div>
+         {/*  Socials  */}
+          <div className="space-y-2">
+            <h3 className="text-xl font-medium">Socials</h3>
+            <a
+              href="https://www.linkedin.com/company/enactus-smit/"
+              className="contact-item"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-8 mt-1"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+              </svg>
+              <span className="">LinkedIn</span>
+            </a>
+            <a
+              href="https://github.com/abhrajitray77"
+              target="_blank"
+              className="contact-item"
+            >
+              <svg
+                viewBox="0 0 512 512"
+                className="w-10 h-10 mt-1"
+                fill="currentColor"
+              >
+                <path d="M256 70.7c-102.6 0-185.9 83.2-185.9 185.9 0 82.1 53.3 151.8 127.1 176.4 9.3 1.7 12.3-4 12.3-8.9V389.4c-51.7 11.3-62.5-21.9-62.5-21.9 -8.4-21.5-20.6-27.2-20.6-27.2 -16.9-11.5 1.3-11.3 1.3-11.3 18.7 1.3 28.5 19.2 28.5 19.2 16.6 28.4 43.5 20.2 54.1 15.4 1.7-12 6.5-20.2 11.8-24.9 -41.3-4.7-84.7-20.6-84.7-91.9 0-20.3 7.3-36.9 19.2-49.9 -1.9-4.7-8.3-23.6 1.8-49.2 0 0 15.6-5 51.1 19.1 14.8-4.1 30.7-6.2 46.5-6.3 15.8 0.1 31.7 2.1 46.6 6.3 35.5-24 51.1-19.1 51.1-19.1 10.1 25.6 3.8 44.5 1.8 49.2 11.9 13 19.1 29.6 19.1 49.9 0 71.4-43.5 87.1-84.9 91.7 6.7 5.8 12.8 17.1 12.8 34.4 0 24.9 0 44.9 0 51 0 4.9 3 10.7 12.4 8.9 73.8-24.6 127-94.3 127-176.4C441.9 153.9 358.6 70.7 256 70.7z" />
+              </svg>
+              <span className="">GitHub</span>
+            </a>
+          </div>
         </section>
       </article>
     </main>
--- components/ProjCard.tsx
+++ components/ProjCard.tsx
@@ -26,14 +26,15 @@ const ProjCard = ({
         <Image src={imgSrc} alt={altText} width={400} />
       </div>
       <div className="flex-col space-y-2 mt-4 lg:ml-10  ">
-        <h2 className="text-xl font-semibold text-gray-800">{name}</h2>
+        <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">{name}</h2>
         <p>{desc}</p>
         <div>
           <h3>Tech Stack:</h3>
           <ul className="flex flex-wrap">
             {tech.map((techItem, index) => (
               <li
-                className=" bg-blue-200 rounded-full mt-2 mr-2 px-2 whitespace-nowrap"
+                className=" bg-blue-200 rounded-full mt-2 mr-2 px-3 whitespace-nowrap dark:text-gray-800
+                py-1"
                 key={index}
               >
                 {techItem}
