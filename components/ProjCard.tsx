be7c27761e6e4abee4b5bc0434f821dc237c0915
abhrajitray77 <abhrajitray77@gmail.com>
2023-06-21 23:50:27 +0530

    Updated projects

--- components/ProjCard.tsx
+++ components/ProjCard.tsx
@@ -22,9 +22,11 @@ const ProjCard = ({
 }: ProjCardProps) => {
   return (
     <div className="grid lg:grid-cols-2  ">
-      <div className="w-auto">
-        <video src={vidSrc} autoPlay loop muted width={400} height={220} />
-      </div>
+      <a href={projLink ? projLink : gitLink} target="_blank">
+        <div className="w-auto cursor-pointer relative">
+          <video src={vidSrc} autoPlay loop muted width={400} height={220} />
+        </div>
+      </a>
       <div className="flex-col space-y-2 mt-4 lg:mt-0 ">
         <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
           {name}
--- utils/projDetails.ts
+++ utils/projDetails.ts
@@ -1,67 +1,78 @@
 const ProjectDetails = [
-    {
-      name: "Enactus SMIT",
-      desc:
-        "The official website of Enactus SMIT built using React.js, TailwindCSS, Framer Motion, and Firebase.",
-      vidSrc:
-        "https://github.com/abhrajitray77/Enactus-smit-final/assets/67530432/b160660a-d241-4e2f-90ad-a7484cbdadb5",
-      projLink: "https://enactus-smit-final.vercel.app/",
-      gitLink: "https://github.com/abhrajitray77/Enactus-smit-final",
-      tech: [
-        "React.js",
-        "TailwindCSS",
-        "Framer-Motion",
-        "React-Router",
-        "Firebase",
-        "EmailJS",
-        "Javascript",
-      ],
-    },
-    {
-      name: "Gpt-3 X Web Clipper Chrome extension",
-      desc:
-        "ReactJS Chrome Extension with GPT-3 integration and Webpage text clipping functionality. (My OpenAI credits for API calls are over)",
-      vidSrc:
-        "https://github.com/abhrajitray77/reactjs-chrome-extension/assets/67530432/22754f00-0901-4126-85ec-211f320dcfff",
-      gitLink: "https://github.com/abhrajitray77/reactjs-chrome-extension",
-      tech: ["React.js", "TailwindCSS", "OpenAI API", "Typescript"],
-    },
-    {
-      name: "GPT-3 Chatbot using OpenAI API",
-      desc:
-        "An AI chatbot built using Next.js 13, Tailwind CSS, and Typescript. Uses the OpenAI API to generate responses. Firebase for Google authentication for multiple users and Firestore for storing the user's chat history. (My OpenAI credits for API calls are over)",
-      vidSrc:
-        "https://github.com/abhrajitray77/PepeGPT/assets/67530432/aacdc9ed-a587-47a6-9f61-c365af078375",
-      projLink: "https://custom-chat-gpt-alpha.vercel.app/",
-      gitLink: "https://github.com/abhrajitray77/PepeGPT",
-      tech: ["Next.js", "TailwindCSS", "OpenAI API", "Firebase", "Typescript"],
-    },
-    {
-      name: "Doctor's Availability Checker",
-      desc:
-        "A site that fetches data from a MongoDB database to show the schedule of doctors from various departments.",
-      vidSrc:
-        "https://github-production-user-asset-6210df.s3.amazonaws.com/67530432/240319234-ccc3a742-ada3-4877-a6ca-5e42ecd95758.mp4",
-      gitLink: "https://github.com/abhrajitray77/appointment_checker",
-      tech: [
-        "React.js",
-        "TailwindCSS",
-        "MongoDB",
-        "Express.js",
-        "Node.js",
-        "Javascript",
-      ],
-    },
-    {
-      name: "Netflix Landing Page Clone",
-      desc:
-        "A Netflix landing page Clone built with Typescript, Next.js, and TailwindCSS that uses the TMDB API to fetch the movie details and posters.",
-      vidSrc:
-        "https://github.com/abhrajitray77/netflix_clone/assets/67530432/7e04df5e-bf01-477d-91ea-9d2b4eb16fb3",
-      projLink: "https://netflix-learn.vercel.app/",
-      gitLink: "https://github.com/abhrajitray77/netflix_clone",
-      tech: ["Next.js", "TailwindCSS", "REST API", "Typescript"],
-    },
-  ];
+  {
+    name: "PixelPaws",
+    desc: "PixelPaws is a game discovery platform that makes it easier for you to find the games that you want to play and also keep track of the ones you're done playing. It uses Appwrite Cloud for Authentication and Databases, and the RAWG API for fetching the game data. You can search for any game, filter by popularity, new releases, and genre, add to your library and save games to your wishlist.",
+    vidSrc:
+      "https://private-user-images.githubusercontent.com/67530432/247648521-9a5002d5-d90c-4526-831f-377bfba06c88.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJrZXkxIiwiZXhwIjoxNjg3MzcxMDk2LCJuYmYiOjE2ODczNzA3OTYsInBhdGgiOiIvNjc1MzA0MzIvMjQ3NjQ4NTIxLTlhNTAwMmQ1LWQ5MGMtNDUyNi04MzFmLTM3N2JmYmEwNmM4OC5tcDQ_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMwNjIxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMDYyMVQxODA2MzZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1jMjY2ZmY3ZTczNTA2N2M5MTI2NzJiM2FhMTE1NTAwMTBiN2JkMmZmMmM4ODA0YmE4MjBmYmMxYmNhNzZmZjEyJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.F4rIYMBFB1atHqpaXksoF3VTU0NvdS2US3rY34DJJog",
+    projLink: "https://pixelpaws.vercel.app/",
+    gitLink: "https://github.com/abhrajitray77/pixel_paws",
+    tech: [
+      "Next.js 13",
+      "TailwindCSS",
+      "REST API",
+      "Typescript",
+      "Appwrite",
+      "RAWG API",
+    ],
+  },
+  {
+    name: "Enactus SMIT",
+    desc: "The official website of Enactus SMIT built using React.js, TailwindCSS, Framer Motion, and Firebase.",
+    vidSrc:
+      "https://github.com/abhrajitray77/Enactus-smit-final/assets/67530432/b160660a-d241-4e2f-90ad-a7484cbdadb5",
+    projLink: "https://enactus-smit-final.vercel.app/",
+    gitLink: "https://github.com/abhrajitray77/Enactus-smit-final",
+    tech: [
+      "React.js",
+      "TailwindCSS",
+      "Framer-Motion",
+      "React-Router",
+      "Firebase",
+      "EmailJS",
+      "Javascript",
+    ],
+  },
+  {
+    name: "Gpt-3 X Web Clipper Chrome extension",
+    desc: "ReactJS Chrome Extension with GPT-3 integration and Webpage text clipping functionality. (My OpenAI credits for API calls are over)",
+    vidSrc:
+      "https://github.com/abhrajitray77/reactjs-chrome-extension/assets/67530432/22754f00-0901-4126-85ec-211f320dcfff",
+    gitLink: "https://github.com/abhrajitray77/reactjs-chrome-extension",
+    tech: ["React.js", "TailwindCSS", "OpenAI API", "Typescript"],
+  },
+  {
+    name: "GPT-3 Chatbot using OpenAI API",
+    desc: "An AI chatbot built using Next.js 13, Tailwind CSS, and Typescript. Uses the OpenAI API to generate responses. Firebase for Google authentication for multiple users and Firestore for storing the user's chat history. (My OpenAI credits for API calls are over)",
+    vidSrc:
+      "https://github.com/abhrajitray77/PepeGPT/assets/67530432/aacdc9ed-a587-47a6-9f61-c365af078375",
+    projLink: "https://custom-chat-gpt-alpha.vercel.app/",
+    gitLink: "https://github.com/abhrajitray77/PepeGPT",
+    tech: ["Next.js", "TailwindCSS", "OpenAI API", "Firebase", "Typescript"],
+  },
+  {
+    name: "Doctor's Availability Checker",
+    desc: "A site that fetches data from a MongoDB database to show the schedule of doctors from various departments.",
+    vidSrc:
+      "https://github-production-user-asset-6210df.s3.amazonaws.com/67530432/240319234-ccc3a742-ada3-4877-a6ca-5e42ecd95758.mp4",
+    gitLink: "https://github.com/abhrajitray77/appointment_checker",
+    tech: [
+      "React.js",
+      "TailwindCSS",
+      "MongoDB",
+      "Express.js",
+      "Node.js",
+      "Javascript",
+    ],
+  },
+  {
+    name: "Netflix Landing Page Clone",
+    desc: "A Netflix landing page Clone built with Typescript, Next.js, and TailwindCSS that uses the TMDB API to fetch the movie details and posters.",
+    vidSrc:
+      "https://github.com/abhrajitray77/netflix_clone/assets/67530432/7e04df5e-bf01-477d-91ea-9d2b4eb16fb3",
+    projLink: "https://netflix-learn.vercel.app/",
+    gitLink: "https://github.com/abhrajitray77/netflix_clone",
+    tech: ["Next.js", "TailwindCSS", "REST API", "Typescript"],
+  },
+];
 
 export default ProjectDetails;
