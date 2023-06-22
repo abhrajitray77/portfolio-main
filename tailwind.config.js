4d2952b5eb618ae4dcd5fb57d94af7832d97bf20
abhrajitray77 <abhrajitray77@gmail.com>
2023-06-22 11:05:25 +0530

    anim performance improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-      "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- ],
+  ],
   theme: {
     extend: {
       animation: {
-        blob: "blob 5s infinite",
+        special_blob: "special_blob 6s infinite ease-in-out",
       },
       keyframes: {
-        blob: {
-          "0%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": {
-            transform: "translate(30px, -50px) scale(1.1)",
-          },
-          "66%": {
-            transform: "translate(-20px, 20px) scale(0.9)",
-          },
-          "100%": {
-            transform: "tranlate(0px, 0px) scale(1)",
-          },
+        special_blob:{
+        "33%": {
+            transform: "translate(200px, 0px) scale(1.3)",
         },
+        "50%": {
+            transform: "translate(120px, 50px) scale(1.1)",
+        },
+        "60%": {
+            transform: "translate(-100px, 20px) scale(1.0)",
+        },
+        "76%": {
+            transform: "translate(-320px, 10px) scale(0.9)",
+        },
+        "86%": {
+            transform: "translate(-100px, 40px) scale(0.9)",
+        },
+        }
       },
     },
   },
   plugins: [
-       // ...
-       require('tailwind-scrollbar')({ nocompatible: true }),
+    // ...
+    require("tailwind-scrollbar")({ nocompatible: true }),
   ],
-}
+};
