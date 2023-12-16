8a98bad049c69d61c4a233b5c2c21f2677c969fe
abhrajitray77 <abhrajitray77@gmail.com>
2023-12-17 00:15:01 +0530

    config undo

--- next.config.js
+++ next.config.js
@@ -4,19 +4,6 @@ const nextConfig = {
     domains: ['img.shields.io'],
     dangerouslyAllowSVG: true,
   },
-
-  async rewrites() {
-    return [
-      {
-        source: "/blog",
-        destination: "https://starter-kit-drab-seven.vercel.app/blog", 
-      },
-      {
-        source: "/blog/:path*",
-        destination: "https://starter-kit-drab-seven.vercel.app/blog/:path*",
-      },
-      ];
-  },
 }
 
 module.exports = nextConfig
